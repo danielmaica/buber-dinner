af09b0bdb512fbce4d2ed4466ae70b0e7fa0ff30
danielmaica <danielmaica.dev@gmail.com>
2025-05-27 17:51:21 -0300

    remover assembly

--- BuberDinner.Application/Common/Interfaces/Authentication/IJwtTokenGenerator.cs
+++
@@ -1,7 +0,0 @@
-namespace BuberDinner.Application.Common.Interfaces.Authentication
-{
-    public interface IJwtTokenGenerator
-    {
-        string GenerateToken(Guid userId, string email, string firstName, string lastName);
-    }
-}
